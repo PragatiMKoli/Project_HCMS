b8ad948d57912e9390345fdb0a1202a7feb60dc7
mahadev koli <pragatimkoli0@gmail.com>
2024-03-01 23:50:53 +0530

    01-03-2024

---
+++ HCMS_Backend-1/src/main/java/com/amigos/demo/HcmsBackend1Application.java
@@ -0,0 +1,13 @@
+package com.amigos.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcmsBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcmsBackend1Application.class, args);
+	}
+
+}
